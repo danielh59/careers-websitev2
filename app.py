7cf8d1a83c5d962cd87a6a54194b7ad4a82e0a84
tak59 <dev.hoyte@gmail.com>
2023-11-24 19:32:35 -0500

    made helpers and jobpage

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -19,6 +19,14 @@ def job_listings():
 def listings():
       jobs = load_jobs_from_db()
       return jsonify(jobs)
+
+@app.route("/listing/<id>")
+def show_listing(id):
+      job = load_job_from_db(id)
+      if not job:
+            return "Listing Not Found", 404
+      return render_template('listingpage.html', job=job)
+
 
 print(__name__)
 if __name__ == "__main__":
